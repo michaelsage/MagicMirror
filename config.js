a7960860115fab236468b9c1bed802cb8cd9889a
michaelsage <michael@theangel.net>
2018-09-23 16:25:39 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+If you already have my MagicMirror Image you will need to add the following lines to your config.js
+
+Type nano MagicMirror/config/config.js to edit the file
+
+After the this block
+
+ {
+                        module: "compliments",
+                        position: "lower_third"
+                },
+
+Paste or type the following:
+
+{
+                        module: "currentweather",
+                        position: "top_right",
+                        config: {
+                                locationID: "",  //ID from http://www.openweathermap.org/help/city_list.txt
+                                appid: ""
+                        }
+                },
+                {
+                        module: "weatherforecast",
+                        position: "top_right",
+                        header: "Weather Forecast",
+                        config: {
+                                locationID: "",  //ID from http://www.openweathermap.org/help/city_list.txt
+                                appid: ""
