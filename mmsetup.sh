c2f4a914439bd7c4a1a64165f20a38c17325de69
michaelsage <michael@theangel.net>
2018-09-23 16:22:54 +0100

    Update mmsetup.sh

--- mmsetup.sh
+++ mmsetup.sh
@@ -9,3 +9,12 @@ echo " "
 echo -e "\e[1;31mYou will need your OpenWeatherMap API key (sign up here: https://home.openweathermap.org/)\e[0m"
 echo -e "\e[1;31mYou will also need to find your LocationID  (go to https://openweathermap.org and find your city then copy the numbers from the end of the URL)\e[0m"
 read -n 1 -s -r -p "Press any key to continue"
+# Getting and writing variables to config.js
+echo
+echo Please enter your API Key:
+read myappid
+echo Please enter your location ID:
+read mylocid
+sed "s/appid\: \"/&"${myappid}"/" config.js -i
+sed "s/locationID\: \"/&"${mylocid}"/" config.js -i
+
